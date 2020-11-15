fc791161bd2d624fc4a1442d398c8137ce74281c
loczylevi <loczy2003@gmail.com>
2020-11-15 17:56:57 +0000

    Initial commit

---
+++ F0005b.py
@@ -0,0 +1,7 @@
+jelszo = ('tüzesviz')
+tipp = input('Kérem uram a jelszót: ')
+
+if jelszo == tipp:
+   print('Ön beléphet jó szorakozást!')
+else:
+   print('A jelszó helytelen kérjük probálja meg késöbb hehehe! ')
---
+++ main.py
@@ -0,0 +1,1 @@
+import F0005b
